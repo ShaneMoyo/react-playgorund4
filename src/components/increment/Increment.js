ec57db53d8e77ac875f5ac0328e6b8b07fc50773
shanemoyo <smoyo@pdx.edu>
2020-06-20 16:21:32 -0700

    add secnond button.

--- src/components/increment/Increment.js
+++ src/components/increment/Increment.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'; 
 
 export default function Increment() { 
-    const [value, setValue ] = useState(0); 
+    const [value1, setValue1 ] = useState(0); 
+    const [value2, setValue2 ] = useState(0); 
 
-    const handleIncrementClick = () => { 
-        setValue(value+1); 
+    const handleIncrementClick1 = () => { 
+        setValue1(value1+1); 
     }
 
-    const handleDecrementClick = () => { 
-        setValue(value-1);
+    const handleDecrementClick1 = () => { 
+        setValue1(value1-1);
+    }
+
+    const handleIncrementClick2 = () => { 
+        setValue2(value2+1); 
+    }
+
+    const handleDecrementClick2 = () => { 
+        setValue2(value2-1);
     }
 
     
         return(
             <>
-                <h2>Value: {value}</h2> 
-                <button onClick={handleIncrementClick}>Increment + </button>
-                <button onClick={handleDecrementClick}>Decrement - </button>
+                <h2>Value1: {value1}</h2> 
+                <button onClick={handleIncrementClick1}>Increment + </button>
+                <button onClick={handleDecrementClick1}>Decrement - </button>
+                <h2>Value2: {value2}</h2> 
+                <button onClick={handleIncrementClick2}>Increment + </button>
+                <button onClick={handleDecrementClick2}>Decrement - </button>
             </>
         )
     
